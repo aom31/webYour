578243c9d8a4f448cc0da3c26e9a924bf92dbf84
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-12 18:21:40 +0700

    test

--- nodeserver.js
+++ nodeserver.js
@@ -2,14 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//define variable data 
+var id = 123;
+var statuse = "single";
+var users = [
+    {name : "thamakorn" , lname: "ketnoi ", birth_day : 2000 },
+    {name : "tha" , lname : "kot" , birth_day: 2011},
+    {name: "asd" , lname: "thjaj" , birth_day: 2001}
+];
 
 //set view engine to els 
 app.set('view engine', 'ejs');
 //use res,render to load up an ejs view file 
-app.get('/homepage', function(req, res) {
-    res.render('indexejs')
+// app.get('/homepage', function(req, res) {
+//     res.render('indexejs')
 
+// })
+app.get('/insertfront' , function(req, res) {
+    res.render('indexejs',  {userid : id, statusl : statuse , userlist : users}) //send value from front to back
 })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
